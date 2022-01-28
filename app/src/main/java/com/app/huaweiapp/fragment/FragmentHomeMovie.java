b1ac19d9a247c94fc52cbe5dc73e4854f8b19967
Kendrick <kendrick0120@gmail.com>
2022-01-28 22:31:20 +0700

    fix location service

--- app/src/main/java/com/app/huaweiapp/fragment/FragmentHomeMovie.java
+++ app/src/main/java/com/app/huaweiapp/fragment/FragmentHomeMovie.java
@@ -31,6 +31,7 @@ import com.app.huaweiapp.utils.Credentials;
 import com.huawei.hmf.tasks.OnFailureListener;
 import com.huawei.hmf.tasks.OnSuccessListener;
 import com.huawei.hmf.tasks.Task;
+import com.huawei.hmf.tasks.Tasks;
 import com.huawei.hms.common.ApiException;
 import com.huawei.hms.common.ResolvableApiException;
 import com.huawei.hms.location.FusedLocationProviderClient;
@@ -58,6 +59,7 @@ public class FragmentHomeMovie extends Fragment {
     Vector<Movie> topRatedMovieList = new Vector<>();
     Vector<Movie> popularMovieList = new Vector<>();
     View v;
+    String countryCode = "";
 
     public static final String TAG = "LocationUpdatesCallback";
     LocationCallback mLocationCallback;
@@ -86,129 +88,6 @@ public class FragmentHomeMovie extends Fragment {
         requestLocationUpdatesWithCallback();
 
         return v;
-    }
-
-    protected void getCountryCode(Double latitude, Double longitude){
-
-        GeoApiEndPoint geoApi = GeoApiService.getGeoApi();
-        Call<com.app.huaweiapp.model.Location> responseCall =
-                geoApi.getLocation("json",latitude,longitude
-                );
-        Log.d("Loglog", "Link " + responseCall.request().url().toString());
-        responseCall.enqueue(new Callback<com.app.huaweiapp.model.Location>() {
-            @Override
-            public void onResponse(Call<com.app.huaweiapp.model.Location> call, Response<com.app.huaweiapp.model.Location> response) {
-                if(response.code() == 200){
-                    com.app.huaweiapp.model.Location location = response.body();
-                    com.app.huaweiapp.model.Address adr = location.getAddress();
-                    String countryCode;
-                    countryCode = adr.getCountryCode().toUpperCase();
-                    Log.d("Loglog", "CC >>>>> " + countryCode);
-                }
-                else{
-                    try{
-                        Log.d("Loglog", "Error " + response.errorBody().toString());
-                    }catch (Exception e){};
-                }
-            }
-
-            @Override
-            public void onFailure(Call<com.app.huaweiapp.model.Location> call, Throwable t) {
-
-            }
-        });
-    }
-
-    void setUpLocation(){
-        RequestLocationPermission.requestLocationPermission(v.getContext());
-
-        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(v.getContext());
-        mSettingsClient = LocationServices.getSettingsClient(v.getContext());
-        mLocationRequest = new LocationRequest();
-        // Sets the interval for location update (unit: Millisecond)
-//        mLocationRequest.setInterval(5000);
-        // Sets the priority
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        if (null == mLocationCallback) {
-            mLocationCallback = new LocationCallback() {
-                @Override
-                public void onLocationResult(LocationResult locationResult) {
-                    if (locationResult != null) {
-                        List<Location> locations = locationResult.getLocations();
-                        if (!locations.isEmpty()) {
-                            for (Location location : locations) {
-                                Log.d("Loglog", "Longitude " + location.getLongitude() + " Latitude " + location.getLatitude());
-                                double longitude = location.getLongitude();
-                                double latitude = location.getLatitude();
-                                getCountryCode(longitude, latitude);
-                            }
-                        }
-                    }
-                }
-
-                @Override
-                public void onLocationAvailability(LocationAvailability locationAvailability) {
-                    if (locationAvailability != null) {
-                        boolean flag = locationAvailability.isLocationAvailable();
-                        Log.i(TAG, "onLocationAvailability isLocationAvailable:" + flag);
-                    }
-                }
-            };
-        }
-    }
-
-    private void requestLocationUpdatesWithCallback() {
-        try {
-            LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
-            builder.addLocationRequest(mLocationRequest);
-            LocationSettingsRequest locationSettingsRequest = builder.build();
-            // Before requesting location update, invoke checkLocationSettings to check device settings.
-            Task<LocationSettingsResponse> locationSettingsResponseTask = mSettingsClient.checkLocationSettings(locationSettingsRequest);
-            locationSettingsResponseTask.addOnSuccessListener(new OnSuccessListener<LocationSettingsResponse>() {
-                @Override
-                public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
-                    Log.i(TAG, "check location settings success");
-                    mFusedLocationProviderClient
-                            .requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.getMainLooper())
-                            .addOnSuccessListener(new OnSuccessListener<Void>() {
-                                @Override
-                                public void onSuccess(Void aVoid) {
-                                    Toast.makeText(v.getContext(),  "requestLocationUpdatesWithCallback onSuccess", Toast.LENGTH_LONG).show();
-
-                                }
-                            })
-                            .addOnFailureListener(new OnFailureListener() {
-                                @Override
-                                public void onFailure(Exception e) {
-                                    Log.e(TAG,
-                                            "requestLocationUpdatesWithCallback onFailure:" + e.getMessage());
-                                }
-                            });
-                }
-            })
-                    .addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(Exception e) {
-                            Log.i(TAG,  "checkLocationSetting onFailure:" + e.getMessage());
-                            int statusCode = ((ApiException) e).getStatusCode();
-                            switch (statusCode) {
-                                case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
-                                    try {
-                                        //When the startResolutionForResult is invoked, a dialog box is displayed, asking you to open the corresponding permission.
-                                        ResolvableApiException rae = (ResolvableApiException) e;
-                                        rae.startResolutionForResult((Activity) v.getContext(), 0);
-                                    } catch (IntentSender.SendIntentException sie) {
-                                        Log.e(TAG, "PendingIntent unable to execute request.");
-                                    }
-                                    break;
-                                default:
-                                    break;
-                            }
-                        }
-                    });
-        } catch (Exception e) {
-            Log.i(TAG,  "requestLocationUpdatesWithCallback exception:" + e.getMessage());
-        }
     }
 
     void setUpTopRatedRecyclerView(){
@@ -284,5 +163,134 @@ public class FragmentHomeMovie extends Fragment {
         });
     }
 
+    // region Location Kit
 
+    protected void setCountryCode(Double latitude, Double longitude){
+        GeoApiEndPoint geoApi = GeoApiService.getGeoApi();
+        Call<com.app.huaweiapp.model.Location> responseCall =
+                geoApi.getLocation("json",latitude,longitude
+                );
+        Log.d("Loglog", "Link " + responseCall.request().url().toString());
+        responseCall.enqueue(new Callback<com.app.huaweiapp.model.Location>() {
+            @Override
+            public void onResponse(Call<com.app.huaweiapp.model.Location> call, Response<com.app.huaweiapp.model.Location> response) {
+                if(response.code() == 200){
+                    com.app.huaweiapp.model.Location location = response.body();
+                    com.app.huaweiapp.model.Address adr = location.getAddress();
+
+                    countryCode = adr.getCountryCode().toUpperCase();
+                    Log.d("Loglog", "CC >>>>> " + countryCode);
+                }
+                else{
+                    try{
+                        countryCode = "US";
+                        Log.d("Loglog", "Error " + response.errorBody().toString());
+                    }catch (Exception e){};
+                }
+            }
+
+            @Override
+            public void onFailure(Call<com.app.huaweiapp.model.Location> call, Throwable t) {
+
+            }
+        });
+    }
+
+    void setUpLocation(){
+        RequestLocationPermission.requestLocationPermission(v.getContext());
+
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(v.getContext());
+        mSettingsClient = LocationServices.getSettingsClient(v.getContext());
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        if (null == mLocationCallback) {
+            mLocationCallback = new LocationCallback() {
+                @Override
+                public void onLocationResult(LocationResult locationResult) {
+                    if (locationResult != null) {
+                        List<Location> locations = locationResult.getLocations();
+                        if (!locations.isEmpty()) {
+                            for (Location location : locations) {
+                                Log.d("Loglog", "Longitude " + location.getLongitude() + " Latitude " + location.getLatitude());
+                                double latitude = location.getLatitude();
+                                double longitude = location.getLongitude();
+                                setCountryCode(latitude, longitude);
+                            }
+                        }
+                    }
+                }
+
+                @Override
+                public void onLocationAvailability(LocationAvailability locationAvailability) {
+                    if (locationAvailability != null) {
+                        boolean flag = locationAvailability.isLocationAvailable();
+                        Log.d("Loglog", "onLocationAvailability isLocationAvailable:" + flag);
+                    }
+                    else {
+                        Log.d("Loglog", "onLocationAvailability =:" + locationAvailability.getLocationStatus());
+                    }
+                }
+            };
+        }
+    }
+
+    private void requestLocationUpdatesWithCallback() {
+        try {
+            LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+            builder.addLocationRequest(mLocationRequest);
+            LocationSettingsRequest locationSettingsRequest = builder.build();
+            // Before requesting location update, invoke checkLocationSettings to check device settings.
+            Task<LocationSettingsResponse> locationSettingsResponseTask = mSettingsClient.checkLocationSettings(locationSettingsRequest);
+            locationSettingsResponseTask.addOnSuccessListener(new OnSuccessListener<LocationSettingsResponse>() {
+                @Override
+                public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                    Log.i(TAG, "check location settings success");
+                    mFusedLocationProviderClient
+                            .requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.getMainLooper())
+                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Toast.makeText(v.getContext(),  "requestLocationUpdatesWithCallback onSuccess", Toast.LENGTH_LONG).show();
+
+                                    // Stop Location Updates after Success
+                                    if(countryCode != ""){
+                                        mFusedLocationProviderClient.removeLocationUpdates(mLocationCallback);
+                                        Toast.makeText(v.getContext(),  "Country code: " + countryCode, Toast.LENGTH_LONG).show();
+                                    }
+
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(Exception e) {
+                                    Log.e(TAG,"requestLocationUpdatesWithCallback onFailure:" + e.getMessage());
+                                }
+                            });
+                }
+            })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(Exception e) {
+                            Log.i(TAG,  "checkLocationSetting onFailure:" + e.getMessage());
+                            int statusCode = ((ApiException) e).getStatusCode();
+                            switch (statusCode) {
+                                case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                                    try {
+                                        //When the startResolutionForResult is invoked, a dialog box is displayed, asking you to open the corresponding permission.
+                                        ResolvableApiException rae = (ResolvableApiException) e;
+                                        rae.startResolutionForResult((Activity) v.getContext(), 0);
+                                    } catch (IntentSender.SendIntentException sie) {
+                                        Log.e(TAG, "PendingIntent unable to execute request.");
+                                    }
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+                    });
+        } catch (Exception e) {
+            Log.i(TAG,  "requestLocationUpdatesWithCallback exception:" + e.getMessage());
+        }
+    }
+    // endregion
 }
